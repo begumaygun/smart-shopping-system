41435bce6f882501cc833be1bce9e0153e61c165
begumaygun <aygunbegum02@gmail.com>
2025-05-09 00:28:41 +0300

    frontend popular categories dashboard

--- frontend/shoplens-frontend/src/pages/HomePage.jsx
+++ frontend/shoplens-frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import PromoBanner from '../components/PromoBanner';
 import Topbar from '../components/Topbar';
+import TopCategoriesChart from "../components/TopCategoriesChart";
 
 
 const HomePage = () => (
@@ -14,9 +15,14 @@ const HomePage = () => (
       {/* Sağ içerik */}
       <div className="flex-1 p-6 overflow-auto">
         <PromoBanner />
+        <TopCategoriesChart />
         {/* Diğer içerikler */}
       </div>
+      
+      
     </div>
+    
+    
 
   </div>
 );
